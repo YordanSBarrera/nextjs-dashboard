653c590bbb8034c96b0582466ec96a9db07bdb42
Yordan Saldaña <yordansaldanabarrera@gmail.com>
2025-03-04 21:22:58 -0300

    add config next-auth

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -63,8 +63,6 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 name="amount"
                 type="number"
                 step="0.01"
-                // required
-
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
                 aria-describedby="amount-error"
